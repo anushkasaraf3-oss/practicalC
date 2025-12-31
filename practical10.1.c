17264e02ac6eeb8992b6e2df0918ce06d0243765
Anushka Saraf <anushka.03saraf@gmail.com>
2025-12-31 20:21:42 +0530

    Added C practical programs

---
+++ practical10.1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], r, c, i, j;
+    int sum = 0;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    printf("Enter elements of the matrix:\n");
+    for(i = 0; i < r; i++) {
+        for(j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+            sum = sum + a[i][j];
+        }
+    }
+
+    printf("Sum of all elements = %d", sum);
+
+    return 0;
+}
